c70bfa338c30c12163037e861edb1da4cb00096c
Sulabh Nepal <esulabh119@gmail.com>
2025-03-30 10:19:30 +0530

    test

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -18,6 +18,11 @@ class Customer extends Model
         return $this->hasMany(FamilyMember::class);
     }
 
+    public function getNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
     protected static function booted()
     {
         static::addGlobalScope('search', function ($builder) {
